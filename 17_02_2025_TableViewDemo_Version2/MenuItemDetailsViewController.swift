2c6c2eac9eb73f8ccc2dad3dbd08f8ba59b407d1
aishwarya <ashp17star@gmail.com>
2025-03-21 15:30:49 +0530

    Data Pasiing -- from table view cell to details view controller

---
+++ 17_02_2025_TableViewDemo_Version2/MenuItemDetailsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MenuItemDetailsViewController.swift
+//  17_02_2025_TableViewDemo_Version2
+//
+//  Created by Vishal Jagtap on 21/03/25.
+//
+
+import UIKit
+
+class MenuItemDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
